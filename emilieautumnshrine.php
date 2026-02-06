89b055c6edaa8cd0f73fb2cd04717d6c2d9f95a6
delaney <116393831+dellanonsense@users.noreply.github.com>
2026-02-06 09:09:16 -0500

    ea shrine update

--- emilieautumnshrine.php
+++ emilieautumnshrine.php
@@ -28,12 +28,14 @@
             </div>
          <h1> emilie autumn </h1>
         <p><b>emilie autumn</b> is an alternative gothic artist most prevelant in her music discography and primary novel <i>The Asylum For Wayward Victorian Girls</i>.</p>
-        <p> though ea hasn't been active since 2022-3, I adore her art and message. she pushes past the hurdles that come with being a woman in both modern and older society, mental illness, and the perceptions that come alongside "strange" artistic decisions. </p>
+        <p> I adore emilie autumn's art and message. she pushes past the hurdles that come with being a woman in both modern and older society, mental illness, and the perceptions that come alongside "strange" artistic decisions. </p>
         <p> emilie autumn has encouraged me to create my own work and not feel bad or worth less for being "off". I will sing her praises (and the entire lyrical content of "miss lucy had some leeches") as long as I'm online. </p>
         <h2> artwork I've made featuring emilie autumn & the asylum </h2>
         <div class="gallery">
-            <img src="assets/emily with a y.webp">
             <img src="assets/wayward girls scavenger txt.webp">
+            <img src="assets/emilie emily.webp">
+            <img src="assets/im keeping watch.webp">
+            <img src="assets/dont kiss me lyrics.webp">
         </div>
     </div>
 </body>
